1de8e513204b63ebebc2f1cc2ab9764e878fc68a
League Student <league@iMac-15.attlocal.net>
2019-11-24 11:05:42 -0800

    Done with Module 0

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -9,18 +9,42 @@ public class MoreArrayFun {
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
-	
+	public static void Method(String[] arr){
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	public static void Reverse(String[] arr) {
+		for(int i=arr.length;  i>0; i--) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
+	public static void every(String[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			if(i%2==0) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+	
 	
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
+	public static void RandomOrder(String[] arr) {
+		Random rand= new Random();
+		int x= rand.nextInt(arr.length);
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[x]);
+			x= rand.nextInt(arr.length);
+		}
+	}
 	
 	
 }
